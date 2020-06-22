aeba08b9b15be98fed2026ceab05e3952bffd083
AB <23291910+data-88@users.noreply.github.com>
2020-06-22 17:11:20 +0200

    working prismic

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import prismicLogo from '../../images/logo-prismic.svg'
+
+export default () =>
+  <footer className="container">
+    <p>
+      Proudly published by Data 88.
+      <br/>
+    </p>
+  </footer>
